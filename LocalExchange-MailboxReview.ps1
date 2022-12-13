6bca764884996c65b433a7b59512a7b2ce0113bb
JDS300 <70587798+JDS300@users.noreply.github.com>
2022-12-13 09:46:18 -0500

    Add files via upload
    https://o365reports.com/2020/10/21/export-office-365-mailbox-size-report-using-powershell/

---
+++ LocalExchange-MailboxReview.ps1
@@ -0,0 +1,77 @@
+﻿Function Get_MailboxSize
+{
+ $Stats=Get-MailboxStatistics -Identity $UPN
+ $ItemCount=$Stats.ItemCount
+ $TotalItemSize=$Stats.TotalItemSize
+ $TotalItemSizeinBytes= $TotalItemSize –replace “(.*\()|,| [a-z]*\)”, “”
+ $TotalSize=$stats.TotalItemSize.value -replace "\(.*",""
+ $DeletedItemCount=$Stats.DeletedItemCount
+ $TotalDeletedItemSize=$Stats.TotalDeletedItemSize
+
+ #Export result to csv
+ $Result=@{'Display Name'=$DisplayName;'User Principal Name'=$upn;'Mailbox Type'=$MailboxType;'Primary SMTP Address'=$PrimarySMTPAddress;'Archive Status'=$Archivestatus;'Item Count'=$ItemCount;'Total Size'=$TotalSize;'Total Size (Bytes)'=$TotalItemSizeinBytes;'Deleted Item Count'=$DeletedItemCount;'Deleted Item Size'=$TotalDeletedItemSize;'Issue Warning Quota'=$IssueWarningQuota;'Prohibit Send Quota'=$ProhibitSendQuota;'Prohibit send Receive Quota'=$ProhibitSendReceiveQuota}
+ $Results= New-Object PSObject -Property $Result  
+ $Results | Select-Object 'Display Name','User Principal Name','Mailbox Type','Primary SMTP Address','Item Count','Total Size','Total Size (Bytes)','Archive Status','Deleted Item Count','Deleted Item Size','Issue Warning Quota','Prohibit Send Quota','Prohibit Send Receive Quota' | Export-Csv -Path $ExportCSV -Notype -Append 
+}
+
+#Output file declaration 
+ $ExportCSV=".\MailboxSizeReport_$((Get-Date -format yyyy-MMM-dd-ddd` hh-mm` tt).ToString()).csv" 
+
+ $Result=""   
+ $Results=@()  
+ $MBCount=0
+ $PrintedMBCount=0
+ Write-Host Generating mailbox size report...
+
+#Get all mailboxes
+  Get-Mailbox -ResultSize Unlimited | foreach {
+   $UPN=$_.UserPrincipalName
+   $Mailboxtype=$_.RecipientTypeDetails
+   $DisplayName=$_.DisplayName
+   $PrimarySMTPAddress=$_.PrimarySMTPAddress
+   $IssueWarningQuota=$_.IssueWarningQuota -replace "\(.*",""
+   $ProhibitSendQuota=$_.ProhibitSendQuota -replace "\(.*",""
+   $ProhibitSendReceiveQuota=$_.ProhibitSendReceiveQuota -replace "\(.*",""
+   $MBCount++
+   Write-Progress -Activity "`n     Processed mailbox count: $MBCount "`n"  Currently Processing: $DisplayName"
+   if($SharedMBOnly.IsPresent -and ($Mailboxtype -ne "SharedMailbox"))
+   {
+    return
+   }
+   if($UserMBOnly.IsPresent -and ($MailboxType -ne "UserMailbox"))
+   {
+    return
+   }  
+   #Check for archive enabled mailbox
+   if(($_.ArchiveDatabase -eq $null) -and ($_.ArchiveDatabaseGuid -eq $_.ArchiveGuid))
+   {
+    $ArchiveStatus = "Disabled"
+   }
+   else
+   {
+    $ArchiveStatus= "Active"
+   }
+   Get_MailboxSize
+   $PrintedMBCount++
+  }
+
+ #Open output file after execution 
+ If($PrintedMBCount -eq 0)
+ {
+  Write-Host No mailbox found
+ }
+ else
+ {
+  Write-Host `nThe output file contains $PrintedMBCount mailboxes.
+  if((Test-Path -Path $ExportCSV) -eq "True") 
+  {
+   Write-Host `nThe Output file available in $ExportCSV -ForegroundColor Green
+   $Prompt = New-Object -ComObject wscript.shell   
+  $UserInput = $Prompt.popup("Do you want to open output file?",`   
+ 0,"Open Output File",4)   
+  If ($UserInput -eq 6)   
+   {   
+    Invoke-Item "$ExportCSV"   
+   } 
+  }
+ }
